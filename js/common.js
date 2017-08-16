dfe2b3074c2b7a5cbf3dbeb90b52c20b6ad2066a
unknown <515684118@qq.com>
2017-08-16 13:15:25 +0800

    a h5 with h5FullscreenPage

---
+++ js/common.js
@@ -0,0 +1,56 @@
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('5 3=c.b.m,2=c.b.j;$(9(){a(\'.k\',\'8 8\')});9 a(e,4){5 1,i=2/ 3, 6 = f /g;1=i>6?3/ g : 2 /f,$(e).l({"-h-0-d":4,"0-d":4,"-h-0":"7("+1+")",\'0\':"7("+1+")"})}',23,23,'transform|t|docW|docH|n|var|r|scale|center|function|adapterFn|documentElement|document|origin||320|504|webkit||clientWidth|wrap|css|clientHeight'.split('|'),0,{}))
+function addAnimate(){
+	$('.block .ani1').addClass('spread-q');
+	setTimeout(function(){
+		$('.block .ani2').addClass('transparency');
+	},1000);
+	setTimeout(function(){
+		$('.block .ani3').addClass('transparency');
+	},1250);
+	setTimeout(function(){
+		$('.block .ani4').addClass('transparency');
+	},2100);
+	setTimeout(function(){
+		$('.block .ani5').addClass('transparency');
+	},1550);
+	setTimeout(function(){
+		$('.block .ani6').addClass('transparency');
+	},1500);
+	setTimeout(function(){
+		$('.block .ani7').addClass('transparency');
+	},1600);
+	setTimeout(function(){
+		$('.block .ani8').addClass('transparency');
+	},1700);
+	setTimeout(function(){
+		$('.block .ani9').addClass('transparency');
+	},1800);
+	setTimeout(function(){
+		$('.block .ani9-2').addClass('transparency');
+	},1900);
+	setTimeout(function(){
+		$('.block .ani9-3').addClass('transparency');
+	},2000);
+	setTimeout(function(){
+		$('.block .ani10').addClass('transparency');
+	},2000);
+	setTimeout(function(){
+		$('.block .ani11').addClass('slideDown');
+	},2400);
+}
+function addModalAnimate(){
+	setTimeout(function(){
+		$('.modal .ani1').addClass('transparency');
+	},0);
+	setTimeout(function(){
+		$('.modal .ani2').addClass('transparency');
+	},500);
+	setTimeout(function(){
+		$('.modal .ani3').addClass('transparency');
+	},1000);
+
+	setTimeout(function(){
+		$('.modal .ani4').addClass('transparency');
+	},1500);
+
+}
